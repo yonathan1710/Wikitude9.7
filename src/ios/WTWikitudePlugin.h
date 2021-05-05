ed47c55efd8c4ff8753d879c87e911d1a3d3f330
Yonathan Rivas <yonathan.rivas.lagos@everis.com>
2021-05-05 16:44:31 -0400

    Upload

---
+++ src/ios/WTWikitudePlugin.h
@@ -0,0 +1,14 @@
+//
+//  WTWikitudePlugin.h
+//  Wikitude
+//
+//  Copyright (c) 2012 Wikitude. All rights reserved.
+//
+
+#import <Cordova/CDV.h>
+
+@interface WTWikitudePlugin : CDVPlugin
+
++ (NSString *)composeFailingAPIAuthorizationMessageFromError:(NSError *)error;
+
+@end
